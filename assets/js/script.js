1ea5ba25b53b113f277d68070af43a1face854b8
topesoul <topesoul@gmail.com>
2024-03-02 11:47:14 +0000

    created a javascript script to support parallax effect and included file in js folder created to link other java code and link as needed across pages

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+if (document.getElementById("parallaxElementId")) {
+  var scrolled = window.scrollY || window.pageYOffset;
+  var parallaxElement = document.getElementById("parallaxElementId");
+
+  // Define a function to update the parallax effect
+  function updateParallax() {
+    var coords = scrolled * 0.5 + "px";
+    parallaxElement.style.transform = "translateY(" + coords + ")";
+  }
+
+  // Define a function to handle the scroll event
+  function handleScroll() {
+    // Update the scrolled variable
+    scrolled = window.scrollY || window.pageYOffset;
+    // Request the browser to run the updateParallax function
+    requestAnimationFrame(updateParallax);
+  }
+
+  // Add the scroll event listener
+  window.addEventListener("scroll", handleScroll);
+}
